29d178fa6f5c20901513e16c9a2025f741877818
beyonderzbaby <vafffi476@gmail.com>
2026-09-11 23:17:32 +0530

    app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const defaultPosts = [
+  {
+    id: 1,
+    image: "https://images.unsplash.com/photo-1506744038136-46273834b3fb?w=800",
+    views: 1000,
+    likes: 150,
+    comments: 25,
+    shares: 40,
+    saves: 20,
+    reach: 850
+  },
+  {
+    id: 2,
+    image: "
